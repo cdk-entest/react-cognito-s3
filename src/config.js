2f7a9db72d74a139a2b02a2928d3fbe623933b72
entest <hai@entest.io>
2022-12-02 14:20:03 +0000

    test

---
+++ src/config.js
@@ -0,0 +1,10 @@
+
+export const config = {
+  "USERNAME": "",
+  "PASSWORD": "", 
+  "COGNITO_POOL_ID": "ap-southeast-1_ejiKprtnG",
+  "IDENTITY_POOL_ID": "ap-southeast-1:6550ab13-c764-451b-8a68-f4d9a38a57d9",
+  "CLIENT_ID": "5p914g870q1si8ddp4at7sve2e",
+  "REGION": "ap-southeast-1",
+  "BUCKET": "cognito-demo-bucket-392194582387-1" 
+}
