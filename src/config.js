727442100b88b8d70c10b4767d6ee7e33df31d44
entest <hai@entest.io>
2022-12-08 07:04:48 +0000

    clean

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+export const config = {
+  "USERNAME": "",
+  "PASSWORD": "", 
+  "COGNITO_POOL_ID": "",
+  "IDENTITY_POOL_ID": "",
+  "CLIENT_ID": "",
+  "REGION": "$REGION",
+  "BUCKET": "",
+  "API_URL_MESSAGE": "",
+  "API_URL_TEST": "",
+  "API_URL_POLLY": ""
+}
